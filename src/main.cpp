bce6f095e7ab56e83c584d1ca3d4540dd6656dca
guemax <code-mg@mailbox.org>
2023-10-03 21:16:10 +0200

    Add working code

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,49 @@
+/* MIT License
+
+Copyright (c) 2023 guemax
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include "Arduino.h"
+#include <Adafruit_NeoPixel.h>
+
+#define LED_PIN 6
+#define LED_COUNT 70
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  pinMode(13, OUTPUT);
+  strip.begin();
+  strip.show();
 }
 
 void loop() {
-  digitalWrite(13, HIGH);
-  delay(2000);
-  digitalWrite(13, LOW);
-  delay(1000);
+  int raw_hue = analogRead(A0);
+  int raw_sat = analogRead(A1);
+  int raw_val = analogRead(A2);
+
+  uint16_t hue = raw_hue * 64.0616;        // Between 0 and 65535.
+  uint8_t saturation = raw_sat * 0.249267; // Between 0 and 255.
+  uint8_t value = raw_val * 0.249267;      // Between 0 and 255.
+
+  strip.fill(strip.ColorHSV(hue, saturation, value));
+  strip.show();
+  
+  delay(10);
 }
